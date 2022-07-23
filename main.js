621b4cb6d335a93283ba4defb0c23799e79ba696
Ezequiel Lebon <89430954+ezelebon@users.noreply.github.com>
2022-07-23 02:07:42 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnswitch =  document.querySelector('.switch');
+
+btnswitch.addEventListener('click', () => 
+{
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+    
+}); 
